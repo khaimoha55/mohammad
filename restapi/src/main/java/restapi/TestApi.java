15a29c74b97240e02b5f357e5c232cd466353b39
Khaiyum Mohammad <Khaiyum.Mohammad@addisonlee.com>
2018-09-06 12:13:48 +0100

    first push

---
+++ restapi/src/main/java/restapi/TestApi.java
@@ -0,0 +1,17 @@
+package restapi;
+
+public class TestApi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+public void testApi () {
+	
+	System.out.println("this is testApi");
+}
+
+}
+
---
+++ restapi/src/test/java/com/qa/tests/TestingGIt.java
@@ -0,0 +1,18 @@
+package com.qa.tests;
+
+public class TestingGIt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public void testingGIt() {
+		System.out.println();
+	}
+	public void testing() {
+		System.out.println();
+}
+}
+
